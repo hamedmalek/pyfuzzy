379cf4ef0ec92c4031c9f48c14c6070442165229
MarziehSadatEsmaeeli <49365138+MarziehSadatEsmaeeli@users.noreply.github.com>
2019-04-23 04:28:20 -0700

    Update mf2dict.py

--- pyfuzzy/operators/mf2dict.py
+++ pyfuzzy/operators/mf2dict.py
@@ -1,2 +1,18 @@
 def mf2dict(U, func_name, params):
-    return {}
+
+    if not isinstance(U, list):
+        raise TypeError("U should be a list of numbers.")
+    if not isinstance(func_name, str):
+        raise TypeError("func_name should be a string that shows name of the membership function.")
+    if not isinstance(params, list):
+        raise TypeError("Params should be a list of function params.")
+
+    result = dict()
+
+    for x in U:
+
+        value = globals()[func_name](x, params)
+        result[x] = value
+
+
+    return result
