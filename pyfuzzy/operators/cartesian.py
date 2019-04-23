0d6b77b5174173b67a2ec8d9b166a8e96f368ca6
Amir Nakhaei <>
2019-04-23 06:45:48 +0430

    cartesian operator fix

--- pyfuzzy/operators/cartesian.py
+++ pyfuzzy/operators/cartesian.py
@@ -18,19 +18,20 @@ def cartesian(U1, U2, A, B, func_name="min"):
     if not (func_name == "product" or func_name == "min"):
         raise TypeError("func_name should be either 'min' or 'product'")
 
+
     for member in U1:
         if not isinstance(member, int):
-            raise TabError("U1 memebr at index " + str(U1.index(member)) + " should be int")
+            raise TypeError("U1 memebr at index " + str(U1.index(member)) + " should be int")
 
     for member in U2:
         if not isinstance(member, int):
-            raise TabError("U2 memebr at index " + str(U2.index(member)) + " should be int")
+            raise TypeError("U2 memebr at index " + str(U2.index(member)) + " should be int")
 
     for key in U1:
         try:
             value = float(A.get(key, 0))
             if not 0 <= value <= 1:
-                raise TypeError("Value of A(" + str(key) + "): " + str(value) + " should be between [0, 1].")
+                raise ValueError("Value of A(" + str(key) + "): " + str(value) + " should be between [0, 1].")
         except ValueError:
             raise TypeError(A.get(key, 0) + " should be float.")
 
@@ -38,7 +39,7 @@ def cartesian(U1, U2, A, B, func_name="min"):
         try:
             value = float(B.get(key, 0))
             if not 0 <= value <= 1:
-                raise TypeError("Value of B(" + str(key) + "): " + str(value) + " should be between [0, 1].")
+                raise ValueError("Value of B(" + str(key) + "): " + str(value) + " should be between [0, 1].")
         except ValueError:
             raise TypeError(B.get(key, 0) + " should be float.")
 
