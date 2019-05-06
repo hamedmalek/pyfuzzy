7c0bbd6d6d56996d015b36756142a3a6e6e51206
msadeghsh <sadeghshahr@gmail.com>
2019-05-06 16:02:36 +0430

    Test development completed

--- pyfuzzy/operators/cartesian.py
+++ pyfuzzy/operators/cartesian.py
@@ -1,3 +1,6 @@
+# import string
+
+
 def cartesian(U1, U2, A, B, func_name="min"):
     # checking input type for error
     if not isinstance(U1, list):
@@ -18,14 +21,13 @@ def cartesian(U1, U2, A, B, func_name="min"):
     if not (func_name == "product" or func_name == "min"):
         raise TypeError("func_name should be either 'min' or 'product'")
 
-
     for member in U1:
         if not isinstance(member, int):
-            raise TypeError("U1 memebr at index " + str(U1.index(member)) + " should be int")
+            raise TypeError("U1 member at index " + str(U1.index(member)) + " should be int")
 
     for member in U2:
         if not isinstance(member, int):
-            raise TypeError("U2 memebr at index " + str(U2.index(member)) + " should be int")
+            raise TypeError("U2 member at index " + str(U2.index(member)) + " should be int")
 
     for key in U1:
         try:
@@ -49,14 +51,37 @@ def cartesian(U1, U2, A, B, func_name="min"):
             mfA = A.get(memberA, 0.)
             for memberB in U2:
                 mfB = B.get(memberB, 0.)
-                key = (memberA, memberB)    # tuple of members name
-                result[key] = mfA*mfB
+                key = (memberA, memberB)  # tuple of members name
+                result[key] = mfA * mfB
     else:
         for memberA in U1:
             mfA = A.get(memberA, 0.)
             for memberB in U2:
                 mfB = B.get(memberB, 0.)
-                key = (memberA, memberB)    # tuple of members name
+                key = (memberA, memberB)  # tuple of members name
                 result[key] = min(mfA, mfB)
 
     return result
+
+
+
+# u = list(range(10))
+# v = list(range(10))
+# a = {1: 0, 2: 0.2, 3: 0.4, 4: 0.6, 5: 0.8, 6: 1}
+# b = {1:1, 2:0 , 3:0.4 , 9:0.6}
+#
+#
+#
+# # # A = {1: 0, 2: 0.2, 3: 0.4, 4: 0.6, 5: 0.8, 6: 1}
+# # # B = {1: 0, 2: 0.2, 3: 0.4, 4: 0.6, 5: 0.8, 6: 1}
+# # # print(cartesian([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], A, B, "product"))
+# # print(list(string.ascii_lowercase))
+#
+# # u = list(string.ascii_lowercase)
+# # v = list(string.ascii_lowercase)
+# # A = {'a': 0.1, 'b': 0.02}
+# # B = {'a': 0.2, 'b': 0.5, 'c': 0.7}
+# #
+# # print(u)
+# print(cartesian(u, v, a, b, "product"))
+#
